7570bfd40a0e09a7616433cacb78a6e95ab5bba5
Patrick Malik <e11776819@student.tuwien.ac.at>
2023-07-16 17:55:01 +0200

    introduces factor and swap mutation

--- genetic_algorithm/mutation_operator.py
+++ genetic_algorithm/mutation_operator.py
@@ -17,8 +17,16 @@ class MutationOperator:
 
 
 class SwapMutation(MutationOperator):
-    pass
 
+    def mutate(self, population: list[SolutionRepresentation]) -> None:
+        for individual in population:
+            for i in range(len(individual.solution_representation)):
+                if random.random() < self.mutation_rate:
+                    random_gene_index = random.randint(0, len(individual.solution_representation) - 1)
+                    val = individual.solution_representation[i]
+                    individual.solution_representation[i] = individual.solution_representation[random_gene_index]
+                    individual.solution_representation[random_gene_index] = val
+                    
 
 class ZeroMutation(MutationOperator):
 
@@ -30,4 +38,15 @@ class ZeroMutation(MutationOperator):
 
 
 class FactorMutation(MutationOperator):
-    pass
+
+    def __init__(self, min_factor=0.9, max_factor=1.1, mutation_rate=1 / SOLUTION_SIZE):
+        super(FactorMutation, self).__init__(mutation_rate)
+        self.max_factor = max_factor
+        self.min_factor = min_factor
+
+    def mutate(self, population) -> None:
+        for individual in population:
+            for i in range(len(individual.solution_representation)):
+                if random.random() < self.mutation_rate:
+                    factor = random.uniform(self.min_factor, self.max_factor)
+                    individual.solution_representation[i] *= factor
