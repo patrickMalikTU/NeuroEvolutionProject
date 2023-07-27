91a5f5a70f85886021c8df51c813e50881efef6c
Patrick Malik <e11776819@student.tuwien.ac.at>
2023-07-27 09:28:27 +0200

    introduces cross entropy loss as fitness function

--- genetic_algorithm/fitness_calculator.py
+++ genetic_algorithm/fitness_calculator.py
@@ -3,6 +3,7 @@ from abc import abstractmethod
 from math import floor
 
 import torch
+from torch.nn import CrossEntropyLoss
 
 from neural_network.network import LeNetFromPaper
 
@@ -10,7 +11,7 @@ from neural_network.network import LeNetFromPaper
 class FitnessCalculator:
 
     @abstractmethod
-    def __init__(self, fitness_data, sampling_rate=0.1):
+    def __init__(self, fitness_data, sampling_rate=1):
         self.sampling_rate = sampling_rate
         self.fitness_data = fitness_data
 
@@ -48,7 +49,23 @@ class ErrorFitnessCalculator(FitnessCalculator):
         return 100 - accuracy
 
 
-class MeanSquaredErrorOnClasses(FitnessCalculator):
+class CrossEntropyLossFitnessCalculator(FitnessCalculator):
+
+    def __init__(self, fitness_data, sampling_rate=1):
+        super(CrossEntropyLossFitnessCalculator, self).__init__(fitness_data, sampling_rate)
+        self.loss_calc = CrossEntropyLoss()
 
     def calculate_fitness(self, solution_representation: list[float]):
-        raise NotImplementedError('not yet implemented')
+        model = LeNetFromPaper.network_from_weight_list(solution_representation)
+
+        data = self.get_sampled_data()
+        cum_loss = 0
+
+        with torch.no_grad():
+            for images, labels in data:
+                images = images.to(self.device)
+                labels = labels.to(self.device)
+                outputs = model(images)
+                cum_loss += float(self.loss_calc(outputs, labels))
+
+        return cum_loss / len(data)
